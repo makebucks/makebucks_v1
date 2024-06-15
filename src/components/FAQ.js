b35c8755fe284b01eb07483485e272c8e0188367
Pramod hg <pramod.hg2347@gmail.com>
2024-06-15 20:52:46 +0530

    Fixed bugs

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -1,216 +1,275 @@
 import styled from "styled-components";
 import { useEffect, useState } from "react";
-import {faqData} from '../utils/data';
+import { faqData } from "../utils/data";
 import { useNavigate } from "react-router-dom";
-import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
-const FAQ = () =>{
-    const [value, setValue] = useState('');
-    const navigate = useNavigate()
-    const toggleAns =(val)=>{
-        if(value === val){
-            setValue(()=>{
-                val = 'null';
-                return val;
-            })
-        }
-        setValue((oldval) => {   
-            return val;
-          });
+import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
+const FAQ = () => {
+  const [value, setValue] = useState("");
+  const navigate = useNavigate();
+  const toggleAns = (val) => {
+    if (value === val) {
+      setValue(() => {
+        val = "null";
+        return val;
+      });
     }
-    const [questions, setQuestions] = useState(faqData);
-    const [currentQues, setCurrentQues] = useState(0)
-    useEffect(()=>{
-        setQuestions(faqData);
-    },[])
-    const prevSlide = () => {
-        setCurrentQues((oldQues) => {
-          const result = (oldQues - 1 + questions.length) % questions.length;
-          return result;
-        });
-      };
-      const nextSlide = () => {
-        setCurrentQues((oldQues) => {
-          const result = (oldQues + 1) % questions.length;
-          return result;
-        });
-      };
-    return(
-        <>
-            <StyledFAQSection>
-                    <H2>Frequently Asked Questions.</H2>
-                    <P>Most common questions about our mentroship program</P>
-                <AccordianSection>
-               
-                    <section className="slider-container">
-                        {questions.map((quest, qIndex)=>{
-                            const {id, q1, ans1,q2,ans2,q3,ans3} = quest;
-                            return (
-                                <article className="slide" style=
-                                {{transform:`translateX(${100*(qIndex-currentQues)}%)`,
-                                  opacity: qIndex === currentQues ?1:0,
-                                  visibility:qIndex=== currentQues?'visible':'hidden'
-                                }} key={id}>
-                                    
-                                        <Quetion onClick={()=>{toggleAns(q1)}}>
-                                        <DropDownSection>
-                                        <H4 >{q1}</H4><span>{value === q1? '-' : '+'}</span>
-                                        </DropDownSection>
-                                        { value === q1 &&
-                                        <Answer>
-                                            <AnsP>{ans1}{q1=== "Are you a profitable trader ?" && <A href="https://verifiedpnldocs.fyers.in/VerifiedPNL/htmls/aaa841a7-7e26-4714-a04c-bdfefc3a246e.html" target="_blank"
-                rel="noreferrer"> Verified P&L</A>}</AnsP>
-                                        </Answer>
-                                        }
-                                        </Quetion>
-                                        <Quetion onClick={()=>{toggleAns(q2)}}>
-                                        <DropDownSection>
-                                            <H4 >{q2}</H4><span>{value === q2? '-' : '+'}</span>
-                                        </DropDownSection>
-                                        { value === q2 &&
-                                            <Answer>
-                                                <AnsP>{ans2}{ q2 === "Still have more questions on your mind?" && <><A href='https://wa.me/919972470399' target="_blank" rel="norefrrer">WhatsApp</A>, <A href='https://mail.google.com/mail/?view=cm&fs=1&to=makebucks.in@gmail.com' target="_blank"
-                rel="noreferrer">Mail</A> or <A onClick={()=>navigate("/contact-us")}>Form</A>. We will get back to you ;)</> }</AnsP>
-                                            </Answer>
-                                        }
-                                        </Quetion>
-                                        {q3 && <Quetion onClick={()=>{toggleAns(q3)}}>
-                                        <DropDownSection>
-                                            <H4 >{q3}</H4><span>{value === q3? '-' : '+'}</span>
-                                        </DropDownSection>
-                                        {value === q3 &&
-                                            <Answer>
-                                                <AnsP>{ans3}</AnsP>
-                                            </Answer>
-                                        }
-                                        </Quetion>}
-                                
-                                </article>
-                            )
-                        })}
-                         <button type='button' className='prev' onClick={prevSlide}>
-        <FiChevronLeft />
-      </button>
-      <button type='button' className='next' onClick={nextSlide}>
-        <FiChevronRight />
-      </button>
-                    </section>
-                </AccordianSection>
-                </StyledFAQSection>
-        </>
-    )
-}
+    setValue((oldval) => {
+      return val;
+    });
+  };
+  const [questions, setQuestions] = useState(faqData);
+  const [currentQues, setCurrentQues] = useState(0);
+  useEffect(() => {
+    setQuestions(faqData);
+  }, []);
+  const prevSlide = () => {
+    setCurrentQues((oldQues) => {
+      const result = (oldQues - 1 + questions.length) % questions.length;
+      return result;
+    });
+  };
+  const nextSlide = () => {
+    setCurrentQues((oldQues) => {
+      const result = (oldQues + 1) % questions.length;
+      return result;
+    });
+  };
+  return (
+    <>
+      <StyledFAQSection>
+        <H2>Frequently Asked Questions.</H2>
+        <P>Most common questions about our mentroship program</P>
+        <AccordianSection>
+          <section className="slider-container">
+            {questions.map((quest, qIndex) => {
+              const { id, q1, ans1, q2, ans2, q3, ans3 } = quest;
+              return (
+                <article
+                  className="slide"
+                  style={{
+                    transform: `translateX(${100 * (qIndex - currentQues)}%)`,
+                    opacity: qIndex === currentQues ? 1 : 0,
+                    visibility: qIndex === currentQues ? "visible" : "hidden",
+                  }}
+                  key={id}
+                >
+                  <Quetion
+                    onClick={() => {
+                      toggleAns(q1);
+                    }}
+                  >
+                    <DropDownSection>
+                      <H4>{q1}</H4>
+                      <span>{value === q1 ? "-" : "+"}</span>
+                    </DropDownSection>
+                    {value === q1 && (
+                      <Answer>
+                        <AnsP>
+                          {ans1}
+                          {q1 === "Are you a profitable trader ?" && (
+                            <A
+                              href="https://verifiedpnldocs.fyers.in/VerifiedPNL/htmls/aaa841a7-7e26-4714-a04c-bdfefc3a246e.html"
+                              target="_blank"
+                              rel="noreferrer"
+                            >
+                              {" "}
+                              Verified P&L
+                            </A>
+                          )}
+                        </AnsP>
+                      </Answer>
+                    )}
+                  </Quetion>
+                  <Quetion
+                    onClick={() => {
+                      toggleAns(q2);
+                    }}
+                  >
+                    <DropDownSection>
+                      <H4>{q2}</H4>
+                      <span>{value === q2 ? "-" : "+"}</span>
+                    </DropDownSection>
+                    {value === q2 && (
+                      <Answer>
+                        <AnsP>{ans2}</AnsP>
+                      </Answer>
+                    )}
+                  </Quetion>
+                  {q3 && (
+                    <Quetion
+                      onClick={() => {
+                        toggleAns(q3);
+                      }}
+                    >
+                      <DropDownSection>
+                        <H4>{q3}</H4>
+                        <span>{value === q3 ? "-" : "+"}</span>
+                      </DropDownSection>
+                      {value === q3 && (
+                        <Answer>
+                          <AnsP>
+                            {ans3}
+                            {q3 ===
+                              "Still have more questions on your mind?" && (
+                              <>
+                                <A
+                                  href="https://wa.me/919972470399"
+                                  target="_blank"
+                                  rel="norefrrer"
+                                >
+                                  WhatsApp
+                                </A>
+                                ,{" "}
+                                <A
+                                  href="https://mail.google.com/mail/?view=cm&fs=1&to=makebucks.in@gmail.com"
+                                  target="_blank"
+                                  rel="noreferrer"
+                                >
+                                  Mail
+                                </A>{" "}
+                                or{" "}
+                                <A onClick={() => navigate("/contact-us")}>
+                                  Form
+                                </A>
+                                . We will get back to you ;)
+                              </>
+                            )}
+                          </AnsP>
+                        </Answer>
+                      )}
+                    </Quetion>
+                  )}
+                </article>
+              );
+            })}
+            <button type="button" className="prev" onClick={prevSlide}>
+              <FiChevronLeft />
+            </button>
+            <button type="button" className="next" onClick={nextSlide}>
+              <FiChevronRight />
+            </button>
+          </section>
+        </AccordianSection>
+      </StyledFAQSection>
+    </>
+  );
+};
 const A = styled.a`
-  color:#434BFD;
-  text-decoration:underline;
-`
+  color: #434bfd;
+  text-decoration: underline;
+`;
 const H2 = styled.h2`
-    font-size: 6rem;
-    text-align: center;
-    line-height: 5rem;
-    @media screen and (max-width: 460px){
-        font-size: 30px;
-        line-height: 4rem;
-
-  }
-  @media screen and (min-width: 460px) and (max-width: 768px) {
-    font-size: 3rem;
-}
-@media screen and (max-width: 393px){
+  font-size: 6rem;
+  text-align: center;
+  line-height: 5rem;
+  @media screen and (max-width: 460px) {
     font-size: 30px;
     line-height: 4rem;
   }
-  @media screen and (min-width: 768px ){
+  @media screen and (min-width: 460px) and (max-width: 768px) {
+    font-size: 3rem;
+  }
+  @media screen and (max-width: 393px) {
+    font-size: 30px;
+    line-height: 4rem;
+  }
+  @media screen and (min-width: 768px) {
     line-height: 8rem;
   }
-  @media screen and (max-width: 350px){
-    font-size:24px;
-}
-`
+  @media screen and (max-width: 350px) {
+    font-size: 24px;
+  }
+`;
 const P = styled.p`
-    font-size: 1.875rem;
-    width: 41%;
-    text-align: center;
+  font-size: 1.875rem;
+  width: 41%;
+  text-align: center;
 
-    @media screen and (max-width: 460px){
-        font-size: 1.2rem;
-        width: 70%;
-        margin-top:.6rem;
+  @media screen and (max-width: 460px) {
+    font-size: 1.2rem;
+    width: 70%;
+    margin-top: 0.6rem;
   }
   @media screen and (min-width: 460px) and (max-width: 768px) {
     font-size: 1.5rem;
     width: 77%;
-}
-@media screen and (max-width: 393px){
+  }
+  @media screen and (max-width: 393px) {
     font-size: 14px;
     padding-top: 1rem;
   }
-`
+`;
 const StyledFAQSection = styled.div`
-    display: flex;
-    flex-direction: column;
-    width: 100%;
-    height: auto;
-    padding: 8rem 0 0 0;
-    align-items: center;
-    justify-content: center;
-`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: auto;
+  padding: 8rem 0 0 0;
+  align-items: center;
+  justify-content: center;
+`;
 const AccordianSection = styled.div`
   display: flex;
   flex-direction: column;
   gap: 1rem;
   width: 53%;
   margin-top: 1rem;
-  @media screen and (max-width: 460px){
-        width: 84%;
+  @media screen and (max-width: 460px) {
+    width: 84%;
   }
   @media screen and (min-width: 460px) and (max-width: 768px) {
     width: 80%;
-}
-`
+  }
+`;
 const Quetion = styled.div`
   display: flex;
   flex-direction: column;
-  gap: .8rem;
+  gap: 0.8rem;
   background: #fff;
   border-radius: 10px;
   padding: 2rem;
   width: 100%;
   border: double 2px transparent;
   border-radius: 10px;
-  background-image: linear-gradient(#fff, #fff), radial-gradient(circle at left, rgba(67, 75, 253, 1),rgba(255, 255, 255, 1));
+  background-image: linear-gradient(#fff, #fff),
+    radial-gradient(
+      circle at left,
+      rgba(67, 75, 253, 1),
+      rgba(255, 255, 255, 1)
+    );
   background-origin: border-box;
   background-clip: padding-box, border-box;
-  cursor:pointer;
+  cursor: pointer;
 
-@media screen and (max-width:500px){
-  padding:1.5rem;
-}
-`
+  @media screen and (max-width: 500px) {
+    padding: 1.5rem;
+  }
+`;
 const DropDownSection = styled.div`
-    display: flex;
+  display: flex;
   width: 100%;
-  color: #0C0914;
+  color: #0c0914;
   align-items: center;
   justify-content: space-between;
   font-size: 1.5rem;
   border-radius: 4px;
-`
+`;
 const H4 = styled.h4`
-    font-weight: 500;
-    text-align: left;
-    @media screen and (max-width:500px){
-        font-size: 12px;
-    }
-`
+  font-weight: 500;
+  text-align: left;
+  @media screen and (max-width: 500px) {
+    font-size: 12px;
+  }
+`;
 const Answer = styled.div`
-    display: flex;
-   text-align: left;
-   color: #595959;
-`
+  display: flex;
+  text-align: left;
+  color: #595959;
+`;
 const AnsP = styled.p`
-    font-size:14px;
-    @media screen and (max-width:500px){
-      font-size:10px;
-    }
-`
+  font-size: 14px;
+  @media screen and (max-width: 500px) {
+    font-size: 10px;
+  }
+`;
 export default FAQ;
--- src/components/OurApp.js
+++ src/components/OurApp.js
@@ -1,233 +1,254 @@
 import styled from "styled-components";
-import ourApp from '../img/Laptop (1).png'
+import ourApp from "../img/Laptop (1).png";
 import ContactUs from "./ContactUs";
 import { useForm } from "react-hook-form";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 import { submitNotifyMe } from "../services/notifyme";
 
-const OurApp = ()=>{
-    const queryClient = useQueryClient();
-    const {register, handleSubmit, reset} = useForm();
-    const {mutate, isLoading} = useMutation({
-        mutationFn: submitNotifyMe,
-        onSuccess:()=>{
-            toast.success("Submitted successfully :)");
-            queryClient.invalidateQueries({
-                queryKey:["notifyme"]
-            });
-            reset();
-        },
-        onError:(err)=>toast.error(err.message)
-    })
+const OurApp = () => {
+  const queryClient = useQueryClient();
+  const { register, handleSubmit, reset, formState } = useForm();
+  const { errors } = formState;
+  const { mutate, isLoading } = useMutation({
+    mutationFn: submitNotifyMe,
+    onSuccess: () => {
+      toast.success("Submitted successfully :)");
+      queryClient.invalidateQueries({
+        queryKey: ["notifyme"],
+      });
+      reset();
+    },
+    onError: (err) => toast.error(err.message),
+  });
 
-    const onSubmit = (data) =>{
-        mutate(data);
-    }
-    return(
-        <>
-        <OurAppContainer>
-            <StyledHeader>
-            <StyledContainer>
-                <StyledTitle>
-                        <H2>Coming soon.</H2>
-                           
-                    </StyledTitle>
-                <P>Never miss your trading journal</P>
-            </StyledContainer>
-            </StyledHeader>
-            <NotyfyMeDiv>
-                <Img src={ourApp}></Img>
-                <Form onSubmit={handleSubmit(onSubmit)}>
-                    <InputDiv>
-                        <Input placeholder="Email address"  {...register('email')}></Input>
-                        <NotifyButton type="submit" disabled={isLoading}>Notify me</NotifyButton>
-                    </InputDiv>
-                </Form>
-                <CircleOne></CircleOne>
-                <CircleTwo></CircleTwo>
-            </NotyfyMeDiv>
-            
-            <ContactUs/>
-           
-            </OurAppContainer>
-        </>
-    )
-}
+  const onSubmit = (data) => {
+    mutate(data);
+  };
+  return (
+    <>
+      <OurAppContainer>
+        <StyledHeader>
+          <StyledContainer>
+            <StyledTitle>
+              <H2>Coming soon.</H2>
+            </StyledTitle>
+            <P>Never miss your trading journal</P>
+          </StyledContainer>
+        </StyledHeader>
+        <NotyfyMeDiv>
+          <Img src={ourApp}></Img>
+          <Form onSubmit={handleSubmit(onSubmit)}>
+            <InputDiv>
+              <Input
+                type="text"
+                id="email"
+                placeholder="Email address"
+                {...register("email", {
+                  required: "This field is required",
+                  pattern: {
+                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                    message: "Invalid email address",
+                  },
+                })}
+              ></Input>
+              {errors?.email?.message && <Error>{errors.email.message}</Error>}
+              <NotifyButton type="submit" disabled={isLoading}>
+                Notify me
+              </NotifyButton>
+            </InputDiv>
+          </Form>
+          <CircleOne></CircleOne>
+          <CircleTwo></CircleTwo>
+        </NotyfyMeDiv>
+
+        <ContactUs />
+      </OurAppContainer>
+    </>
+  );
+};
 const Form = styled.form`
-    display: flex;
-    justify-content: center;
-    flex-direction: column;
-    width: 100%;
-    align-items:center;
-`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  width: 100%;
+  align-items: center;
+  position: relative;
+  bottom: 5rem;
+`;
 const H2 = styled.h2`
-    font-size: 6rem;
-    background: linear-gradient(92.12deg, #434BFD 26.52%, #FF3737 115.58%);
+  font-size: 6rem;
+  background: linear-gradient(92.12deg, #434bfd 26.52%, #ff3737 115.58%);
   -webkit-text-fill-color: transparent;
   -webkit-background-clip: text;
   background-size: 105% auto;
   letter-spacing: 0;
   margin: 0;
   text-align: center;
-    font-weight: 600;
-    line-height: 7rem;
-    text-align: center;
-  @media screen and (max-width: 460px){
-        font-size: 30px;
-        line-height: 5rem;
+  font-weight: 600;
+  line-height: 7rem;
+  text-align: center;
+  @media screen and (max-width: 460px) {
+    font-size: 30px;
+    line-height: 5rem;
   }
   @media screen and (min-width: 460px) and (max-width: 768px) {
     font-size: 40px;
     line-height: 5rem;
   }
-  @media screen and (max-width: 493px){
+  @media screen and (max-width: 493px) {
     font-size: 30px;
     line-height: 4rem;
   }
-`
+`;
 const P = styled.p`
-    font-size: 1.875rem;
-    margin: 1rem 0 4rem 0;
-    text-align: center;
+  font-size: 1.875rem;
+  margin: 1rem 0 4rem 0;
+  text-align: center;
 
-    @media screen and (max-width: 460px){
-        font-size: 1.2rem;
+  @media screen and (max-width: 460px) {
+    font-size: 1.2rem;
   }
   @media screen and (min-width: 460px) and (max-width: 768px) {
     font-size: 1.5rem;
     margin-bottom: 5rem;
-}
-`
+  }
+`;
 const NotyfyMeDiv = styled.div`
-    position: relative;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    width: 100%;
-    height: auto;
-    margin-bottom: 10rem;
-`
-const InputDiv= styled.div`
-    display: flex;
-    position: relative;
-    padding: 0 1rem;
-    @media screen and (max-width: 500px){
-        width: 30rem;
-    }
-    @media screen and (min-width: 500px) and (max-width: 768px){
-        width: 40rem;
-    }
-    @media screen and (min-width: 768px) and (max-width: 1200px){
-        width: 40rem;
-    }
-    @media screen and (min-width: 1200px){
-        width: 50rem;
-    }
-`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+  height: auto;
+  margin-bottom: 10rem;
+`;
+const InputDiv = styled.div`
+  display: flex;
+  position: relative;
+  padding: 0 1rem;
+  @media screen and (max-width: 500px) {
+    width: 30rem;
+  }
+  @media screen and (min-width: 500px) and (max-width: 768px) {
+    width: 40rem;
+  }
+  @media screen and (min-width: 768px) and (max-width: 1200px) {
+    width: 40rem;
+  }
+  @media screen and (min-width: 1200px) {
+    width: 50rem;
+  }
+`;
 const Input = styled.input`
-    background: #fff;
-    border: 0;
-    border-radius: 30px;
-    padding: 1rem 0 1rem 2rem;
-    width: 100%;
-`
+  background: #fff;
+  border: 0;
+  border-radius: 30px;
+  padding: 1rem 0 1rem 2rem;
+  width: 100%;
+  font-size: 12px;
+`;
+const Error = styled.span`
+  position: absolute;
+  font-size: 10px;
+  color: red;
+  bottom: -20px;
+  left: 21px;
+`;
 const NotifyButton = styled.button`
-        background-color: rgba(67, 75, 253, 1);
-    font-size: 1.2rem;
-    color: #fff;
-    position: absolute;
-    top: 16%;
+  background-color: rgba(67, 75, 253, 1);
+  font-size: 1.2rem;
+  color: #fff;
+  position: absolute;
+  top: 16%;
+  right: 5%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 30px;
+  @media screen and (min-width: 500px) and (max-width: 768px) {
     right: 5%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    border-radius: 30px;
-    @media screen and (min-width: 500px) and (max-width: 768px){
-        right: 5%;
-    }
-    @media screen and (max-width: 768px){
-        top:18%;
-    }
-`
+  }
+  @media screen and (max-width: 768px) {
+    top: 18%;
+  }
+`;
 const CircleOne = styled.div`
-        width: 40rem;
-    height: 40rem;
+  width: 40rem;
+  height: 40rem;
+  border-radius: 100%;
+  position: absolute;
+  z-index: 1;
+  background: rgba(67, 75, 253, 0.1);
+  top: -4%;
+  @media screen and (max-width: 500px) {
+    width: 25rem;
+    height: 25rem;
     border-radius: 100%;
     position: absolute;
     z-index: 1;
     background: rgba(67, 75, 253, 0.1);
-    top: -4%;
-    @media screen and (max-width: 500px){
-        width: 25rem;
-        height: 25rem;
-        border-radius: 100%;
-        position: absolute;
-        z-index: 1;
-        background: rgba(67, 75, 253, 0.1);
-        top: -5%;
-    }
-
-`
+    top: -5%;
+  }
+`;
 const CircleTwo = styled.div`
-    width: 30rem;
-    height: 30rem;
+  width: 30rem;
+  height: 30rem;
+  border-radius: 100%;
+  position: absolute;
+  z-index: 1;
+  background: rgba(67, 75, 253, 0.25);
+  top: 4%;
+  @media screen and (max-width: 500px) {
+    width: 20rem;
+    height: 20rem;
     border-radius: 100%;
     position: absolute;
     z-index: 1;
     background: rgba(67, 75, 253, 0.25);
-    top: 4%;
-    @media screen and (max-width: 500px){
-        width: 20rem;
-        height: 20rem;
-        border-radius: 100%;
-        position: absolute;
-        z-index: 1;
-        background: rgba(67, 75, 253, 0.25);
-        top: 2%;
-    }
-`
+    top: 2%;
+  }
+`;
 const OurAppContainer = styled.div`
-    display: flex;
-    width: 100%;
-    height: 100%;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-`
+  display: flex;
+  width: 100%;
+  height: 100%;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
 const Img = styled.img`
-        width: 50rem;
-    height: 50rem;
-    z-index: 3;
-    position: relative;
-    bottom: 8rem;
+  width: 50rem;
+  height: 50rem;
+  z-index: 3;
+  position: relative;
+  bottom: 8rem;
 
-    @media screen and (max-width: 500px){
-        width: 30rem;
-        height: 30rem;
-        bottom: 5rem;
-    }
-`
+  @media screen and (max-width: 500px) {
+    width: 30rem;
+    height: 30rem;
+    bottom: 5rem;
+  }
+`;
 const StyledHeader = styled.div`
-    width: 100%;
-    display: flex;
-    align-items: flex-start;
-    justify-content: center;
-    margin-top: 10rem;
-`
+  width: 100%;
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+  margin-top: 10rem;
+`;
 const StyledContainer = styled.div`
-    display: flex;
+  display: flex;
   flex-direction: column;
   align-items: center;
   height: auto;
-`
+`;
 const StyledTitle = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-`
-
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
 
 export default OurApp;
